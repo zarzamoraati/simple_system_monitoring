685fd421bf869e95c2aac75230bb18a4bab4e762
zarzamoraati <zarzamoraati@gmail.com>
2025-08-04 13:24:01 -0600

    Turning script into a deamon

--- resource_monitoring_iter2.sh
+++ resource_monitoring_iter2.sh
@@ -10,20 +10,26 @@ resource_log_path=/home/polo/Documentos/VSCode/VSCode/Projects/linux/bash/resour
 
 get_service_name(){
 	
-	echo "nginx mysqld elastic-agent postgresql"
-
+	#echo "nginx mysqld elastic-agent postgresql"
+	local services=("mysqld" "nginx" "elastic-agent" "postgresql")
+	echo "${services[@]}"
 }
+
+
 
 
 resource_monitoring(){
 ## TODO get cpu usage  as int 
 
-cpu_usage=$(top -b n1 | grep "%Cpu" | awk '{print 100 - $8}')
+if ! cpu_usage=$(top -b n1 | grep "%Cpu" | awk '{print 100 - $8}'); then
+	echo "Error failed to get CPU usage at $(date)" >> "${resource_log_path}"
+	return 1
+fi
 echo "${cpu_usage}"
 
 cpu_usage_int=${cpu_usage%.*}
 
-echo ${cpu_usage_int}
+echo "CPU usage: ${cpu_usage_int}" >> "${resource_log_path}"
 
 
 ## TODO get hreshold
@@ -37,14 +43,16 @@ if [[ "${cpu_usage_int}" -ge "${threshold}" ]]; then
 	echo "CPU usage excedeed threshold limit "
 	echo "Prioritizing services ...."
 	## TODO get list of services 
-	service_list=$(get_service_name) 
+	#service_list=$(get_service_name) 
+	read -r -a service_list <<< "$(get_service_name)"
 	echo "${service_list}"
 	
 	##  TODO check  service list is not empty 
-	if [[ "${service_list}" == 0 ]]; then 
+	if [[ "$#{service_list[@]}" -eq 0 ]]; then 
 
 		echo "Any service was founded, nothing to stop ...."
-		exit 0
+		return 0
+		#exit 0
 	else
 		## TODO disabled services
 		echo "CPU usage before disabled services ${cpu_usage}% " >> "${resource_log_path}"
@@ -64,19 +72,25 @@ if [[ "${cpu_usage_int}" -ge "${threshold}" ]]; then
 		cpu_usage_update=$(top -b n1 | grep "%Cpu" | awk '{print 100 - $8}') 
 		sleep 2
 		echo "CPU usage after stop services: ${cpu_usage_update}%" >> "${resource_log_path}"
-		exit 0
+		#exit 0
 	fi
 
 
 else
 	echo "CPU usage operate under threshold , nothing to do..."
-	exit 0 
+	#exit 0 
 fi
 
 
 }
 
-resource_monitoring
+while true; do
+
+resource_monitoring 
+
+sleep 5
+
+done
 
 
 
